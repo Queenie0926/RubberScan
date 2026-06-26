0b1df1e5901b748cbf4536bc237b9bf70a3f31c2
Queenie0926 <q.doringo.545053@umindanao.edu.ph>
2026-06-26 23:32:45 +0800

    database file

--- app/src/main/java/com/example/rubberscan/db/entity/User.kt
+++ app/src/main/java/com/example/rubberscan/db/entity/User.kt
@@ -1,3 +1,15 @@
 package com.example.rubberscan.db.entity
 
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 
+@Entity(
+    tableName = "users",
+    indices = [Index(value = ["email"], unique = true)]
+)
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val email: String,
+    val passwordHash: String
+)
